f8bbb93170dc6d6aaabab11027535c3aaa642196
Alex Leifermann <aleif92@outlook.com>
2025-02-02 16:34:57 -0600

    Changed User class to UserModel

--- obj/Debug/net8.0/TaskManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0db3df44fe3ea8a4a8515125d3e0694b865510c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
